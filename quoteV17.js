686641143a902a4d0ecd4563e3a6165d4b0404b4
Vikrant Bhat <vik.bhat223@gmail.com>
2023-02-12 22:38:56 +0530

    fix: inr value prefill

--- quoteV17.js
+++ quoteV17.js
@@ -232,7 +232,9 @@ function calculatePriceDateAndUpdateUI({
       // RZP payment btn
       step4_rzpPaymentBtn.setAttribute(
         "data-prefill.amount.amount",
-        calculatedTotalPriceINR
+        (
+          enteredWordCount * costMultipliersPerWord[serviceType]["inr"]
+        ).toString() // Note: this is without .toLocaleString() since we do not need the commas
       );
     }
   }
